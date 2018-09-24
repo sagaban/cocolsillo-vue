d47f5043d518dc50a8f0118fd925f83958811f7d
Santiago Bandiera <sagaban@hotmail.com>
2018-09-24 19:48:17 -0300

    Fix production webpack to add plugin

--- vue.config.js
+++ vue.config.js
@@ -7,15 +7,17 @@ module.exports = {
     }
   },
   transpileDependencies: [/[\\/]node_modules[\\/]quasar-framework[\\/]/],
-  configureWebpack: config => {
+  chainWebpack: config => {
     if (process.env.NODE_ENV === "production") {
-      config.plugin("sw-precache-webpack-plugin").use(SWPrecacheWebpackPlugin, {
-        cacheId: "cocolsillo-vue",
-        filename: "service-worker.js",
-        staticFileGlobs: ["dist/**/*.{js,html,css}"],
-        minify: true,
-        stripPrefix: "dist/"
-      });
+      config.plugin("sw-precache-webpack-plugin").use(SWPrecacheWebpackPlugin, [
+        {
+          cacheId: "cocolsillo-vue",
+          filename: "service-worker.js",
+          staticFileGlobs: ["dist/**/*.{js,html,css}"],
+          minify: true,
+          stripPrefix: "dist/"
+        }
+      ]);
     }
   }
 };
